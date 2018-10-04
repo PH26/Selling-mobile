616cc73bda84814b055b297a9632a74a588908e2
Ha <tanthiha201198@gmail.com>
2018-10-04 23:06:18 +0700

    footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,93 @@
 <style>
-	
+	*{
+		width: 100%;
+		margin:0 auto;
+		padding: 0;
+	}
+	footer{
+		background: #474b54;
+	}
+	footer h3{
+		color: #b2b5ba;
+	}
+	footer i{
+		color: #aaaeb7;
+	}
+	footer a{
+		color: #70747d;
+		margin-left: .4em;
+		font-size: 1.16em;
+	}
+	footer a:hover{
+		color: #00e68a; 
+		text-decoration: none;
+	}
+	footer p{
+		color: #70747d;
+	}
+	footer .about{
+		padding-left: 10em;
+	}
+	footer .sup{
+		padding-left: 6em;
+	}
+	footer .get{
+		padding-right: 4.5em;
+	}
 </style>
-<footer class="col-md-12">
-	this is footer
+<footer>
+	<div class="row fixed-bottom">
+		<!--About us-->
+		<div class="col-md-4 about">
+			<h3 class="title">About us</h3>
+			<ul class="list-unstyled">
+		    	<li>
+		    		<i class="fa fa-caret-right"><a href="#">Who we are</a></i>
+				</li>
+		    	<li>
+		    		<i class="fa fa-caret-right"><a href="#">Blog</a></i>
+		    	</li>
+		    	<li>
+		    		<i class="fa fa-caret-right"><a href="#">Moodle Themes</a></i>
+		    	</li>
+		    	<li>
+		    		<i class="fa fa-caret-right"><a href="#">Contact</a></i>
+		    	</li>
+			</ul>
+		</div>
+		
+		<!--Support-->
+		<div class="col-md-4 sup">
+			<h3 class="title">Support</h3>
+			<ul class="list-unstyled">
+			    <li>
+			    	<i class="fa fa-caret-right"><a href="#">Help</a></i>  
+			    </li>
+			    <li>
+			    	<i class="fa fa-caret-right"><a href="#">FAQs</a></i>
+			    </li>
+			    <li>
+			    	<i class="fa fa-caret-right"><a href="#">Terms of services</a></i>
+			    </li>
+			    <li>
+			    	<i class="fa fa-caret-right"><a href="#">Privacy</a></i>
+			    </li>
+			</ul>
+		</div>
+
+		<!--Get in touch-->
+		<div class="col-md-4 get">
+			<h3 class="title">Get in touch</h3>
+			<p>
+				Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+			</p>                     
+			<p class="tel">
+				<i class="fa fa-phone"><a href="tel:+08001234567">032 666 3287</a></i>	
+			</p>
+			<p class="email">
+				<i class="fa fa-envelope-o"><a href="#">sophia@website.com</a></i>	
+			</p>
+		</div>
+		
+	</div>
 </footer>
