0a4cc37724389074c02e219a740d599e7711a4e9
Ha <tanthiha201198@gmail.com>
2018-10-09 22:55:24 +0700

    view mails

--- resources/views/emails/activation.blade.php
+++ resources/views/emails/activation.blade.php
@@ -1,2 +1,13 @@
-Wellcome, {{ $name }}
-Please active your account : {{ url('user/activation', $link)}}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Activation</title>
+</head>
+<body>
+	<p>Wellcome,<b>{{ $name }}</b> </p>
+	Please active your account:
+	<a href="{{ url('user/activation', $link)}}">{{ url('user/activation', $link)}}</a> 
+	<p>Thanks!</p>
+</body>
+</html>
